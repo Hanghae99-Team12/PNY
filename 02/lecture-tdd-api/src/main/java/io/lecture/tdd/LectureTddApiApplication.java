9b7ac614271a8dc44b0238ec67b617b11de1bbae
soumunda8 <soumunda8@naver.com>
2024-10-02 10:25:19 +0900

    CREATE : Week02 Project

---
+++ 02/lecture-tdd-api/src/main/java/io/lecture/tdd/LectureTddApiApplication.java
@@ -0,0 +1,13 @@
+package io.lecture.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureTddApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureTddApiApplication.class, args);
+	}
+
+}
---
+++ 02/lecture-tdd-api/src/test/java/io/lecture/tdd/LectureTddApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lecture.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureTddApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
